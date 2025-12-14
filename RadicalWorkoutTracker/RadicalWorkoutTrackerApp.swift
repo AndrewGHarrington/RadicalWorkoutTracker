bdb17726498cd374b1ddbed0eb03f9a565a9675d
Andrew Harrington <aghdesigns@icloud.com>
2025-12-13 22:14:58 -0800

    Initial Commit

---
+++ RadicalWorkoutTracker/RadicalWorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadicalWorkoutTrackerApp.swift
+//  RadicalWorkoutTracker
+//
+//  Created by Andrew Harrington on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct RadicalWorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
